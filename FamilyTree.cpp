69e2bdf914d47bf9ac24baf7a3b30c09740bf37c
moriag <47932261+moriag@users.noreply.github.com>
2020-04-23 11:39:52 +0300

    Add files via upload

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -3,134 +3,166 @@ using namespace std;
 string grn="grand",gre="great-";
 namespace family{
     
-    bool Tree::addFatherRe(string name, string father){
-        if(name.compare(root)==0&&_father!=NULL)throw exception();
-        if(this->_father!=NULL && (this->_father->addFatherRe(name,father)==true)){return true;}
-        
-        if(this->_father==NULL && this->root.compare(name)==0){
-            
-            _father= new Tree(father);
-            cout<<"  father "+father+"  "+_father->root;
-            // printf("true");
-            return true;
-        }
-        if(_mother!=NULL && true==(_mother->addFatherRe(name,father))){return true;}
-        printf("  false  ");
-        return false;
-    }
-    bool Tree::addMotherRe(string name, string mother){
-        if(_mother!=NULL&&name.compare(root)==0)throw "mmmm";
-        if(_mother!=NULL && true==(_mother->addMotherRe(name,mother))){return true;}
-        if(_mother==NULL && root==name){
-            _mother= new Tree(mother);
-            // printf("  new  ");
-            cout<<"  mother "+mother+"  "+_mother->root;
 
+    bool Tree::addFatherRe(string name, string father){
+
+        if(_father==NULL && root==name){//found place to add
+            _father= new Tree(father);//add father  
             return true;
         }
+        //search on father's side
+        if(_father!=NULL && (_father->addFatherRe(name,father)))return true;
+        //search on mother's side
+        if(_mother!=NULL && (_mother->addFatherRe(name,father)))return true;
+        //did not find
+        return false; 
+    }
+    
+    
+    bool Tree::addMotherRe(string name, string mother){
         
-        if(_father!=NULL && true==(_father->addMotherRe(name,mother))){return true;}
-        printf("  false  ");
+        if(_mother==NULL && root==name){//found place to add
+            _mother= new Tree(mother);//add mother
+            return true;
+        }
+         //search on mother's side
+        if(_mother!=NULL && (_mother->addMotherRe(name,mother)))return true;
+        //search on father's side
+        if(_father!=NULL && (_father->addMotherRe(name,mother)))return true;
+        //did not find
         return false;
     }
+    
+    
     bool Tree::removeRe(string& name){
-        if(_mother&&_mother->root==name){
-            delete _mother;
+        if(_mother&&_mother->root==name){//found name
+            delete _mother;//remove "name"
+            _mother=NULL;
             return true;
         }
-        if(_father&&_father->root==name){
-            delete _father;
+        if(_father&&_father->root==name){//found name
+            delete _father;//remove "name"
+            _father=NULL;
             return true;
         }
-        if(_mother!=NULL && _mother->removeRe(name)==true)return true;
-        if(_father!=NULL && _father->removeRe(name)==true)return true;
-        return false;
+        //search on mother's side
+        if(_mother!=NULL && _mother->removeRe(name))return true;
+        //search on father's side
+        if(_father!=NULL && _father->removeRe(name))return true;
+        return false;//did not find
     }
+    
+    
     string Tree::findRe(int great, bool mother){
         string ans="";
-        if(great>0){
+        if(great>0){//not yet the generation to search in
+        //go up a generation to mother
             if(_mother!=NULL)ans= this->_mother->findRe(great-1,mother);
+        //if not found go up a generation to father 
             if(_father!=NULL && ans.empty())ans= _father->findRe(great-1,mother);
-            cout<<" - "+ans+" - ";
+            //not found
             return ans;
         }
-        if(mother&&_mother!=NULL)return _mother->root;
-        if(!mother&&_father!=NULL)return _father->root;
-        return ans;
+        //in generation to search
+        if(mother&&_mother!=NULL)return _mother->root;//if looking for a mother
+        if(!mother&&_father!=NULL)return _father->root;//if looking for a father
+        return ans;//not found
+    }
+    
+    //constructor 
+    Tree::Tree(string name):_father(NULL),_mother(NULL),root(name){;}
+    
+    
+    Tree::~Tree(){
+        if(_mother!=NULL)delete _mother;
+        _mother=NULL;
+        if(_father!=NULL)delete _father;
+        _father=NULL;
     }
 
-
-    Tree::Tree(string name):_father(NULL),_mother(NULL),root(name){;}
 
     Tree& Tree::addFather(string name, string father){
-        printf("out");
-        bool b=this->addFatherRe(name,father);
-        printf("%i",b);
-        if(b==true)return *this;
-       
-        printf("falseout");
-        __throw_runtime_error("c");
+        //go to boolean recursion
+        if(this->addFatherRe(name,father)==true)return *this;
+        __throw_runtime_error("cant add father");
+    }
     
-    }
+    
     Tree& Tree::addMother(string name, string mother){
-        printf("outm");
+        //go to boolean recursion 
         if(true==(this->addMotherRe(name,mother)))return *this;
-        __throw_runtime_error("c");
+        __throw_runtime_error("cant add mother");
     }
+
+
     void Tree::remove(string name){
-        if(root.compare(name)==0){
-            delete this;
-            return;
-        }
-        if(removeRe(name))return;
+        if(removeRe(name))return;//go to recursion
         throw exception();
     }
-    void Tree::display(){;
+    
+    
+    void Tree::display(){
+        cout << "Name: " <<root;
+        if(_father!=NULL)cout<<"\tFather: "<<_father->root;
+        if(_mother!=NULL)cout<<"\tMother: "<<_mother->root;
+        cout<<endl;
+        if(_father!=NULL)_father->display();
+        if(_mother!=NULL)_mother->display();
     }
+
+    
+    
     string Tree::relation(string name){
-        cout<<"   "+root+"   ";
-        if(this->_mother!=NULL && _mother->root==name){cout<<" mom "<<_mother->root;return "mother";}
-        if(this->_father!=NULL && _father->root==name){cout<<" pop "<<_father->root;return "father";}
+        
+        if(_mother!=NULL && _mother->root==name)return "mother";//found name..
+        if(_father!=NULL && _father->root==name)return "father";//
         string ans;
         if(this->_mother!=NULL){
-            ans=_mother->relation(name);
-            cout<<ans+"  here!!  ";
-            if(ans.length()==6) return grn+ans;
-            if(ans[0]=='g')return gre+ans; 
+            ans=_mother->relation(name);//relation to mother
+
+            if(ans.length()==6) return grn+ans;//grandmother
+            if(ans[0]=='g')return gre+ans;//grate-...grandmother 
         }
         if(this->_father != NULL){
-            ans=_father->relation(name);
-            if(ans.length()==6)return grn+ans;
-            if(ans[0]=='g')return gre+ans; 
+            ans=_father->relation(name);//relation to father
+
+            if(ans.length()==6)return grn+ans;//grandfather
+            if(ans[0]=='g')return gre+ans; //great-...grandfather
         }
-        if(root==name)return "me";
-        cout<<"error-"+ans+"- ";
-        if(ans.empty())ans="unrelated";
-        return ans;
+        if(root==name)return "me";//found me
+     
+        return "unrelated";//didnt find
+        
     }
+
+
     string Tree::find(string relation){
         string ans="";
         int great,grand=0;
         bool father=false,mother=false;
-        for(great=0;relation.find_first_of("great-")==0;great++){
-            relation=relation.substr(5);}
-        if(relation.find_first_of("grand")==0){
-            grand++;
-            relation=relation.substr(4);
+        //count generations to go back 
+        for(great=0;relation.find("great-")==0;great++){
+            relation=relation.erase(0,6);
         }
-        if(relation.length()==6){
-            if(relation.compare("mother")==0)mother=true;
-            else if(relation.compare("father")==0)father=true;
-            if((mother||father)&&(grand=1||great==0)){
-                ans+= this->findRe(great,mother);
-                cout<<" f "+ans+" f "<<!ans.empty();
-                if(!ans.empty())return ans;
-                cout<<" ! "+ans+" ! ";
+        if(relation.find("grand")==0){
+            grand++;
+            relation=relation.erase(0,5);
+        }
+        if(relation.length()==6){//length of mother\father
+            
+            if(relation=="mother")mother=true;
+            else if(relation=="father")father=true;
+            //need one parent, if no grand, #"great-" must be 0
+            if((mother||father)&&(grand==1||great==0)){
+                ans= this->findRe(great+grand,mother);
+                        // #generations  , gender of last in line
+            
+                if(!ans.empty())return ans;//found in tree
+               
             }   
         }
-        cout<<" ff "+ans+" f ";
-        if(ans.empty())throw "k";
-        // return ans;
+  
+        throw "not found in tree";//including wrong format input
     
     }
    
--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -3,6 +3,7 @@
 using namespace std;
 namespace family{
     class Tree{
+       
         string root;
         Tree* _father;
         Tree* _mother;
@@ -11,6 +12,7 @@ namespace family{
         bool addMotherRe(string name, string mother);
         bool removeRe(string& name);
         public:
+        ~Tree();
         Tree(string name);
         Tree& addFather(string name, string father);
         Tree& addMother(string name, string mother);
